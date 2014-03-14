d54bd2a1ed580c28a56bd4a2a836d6dcfb93e8fe
Marion Ano <meaola@gmail.com>
2014-03-13 20:05:23 -0500

    initial commit

---
+++ TicTacToe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TicTacToe
+//
+//  Created by Marion Ano on 3/13/14.
+//  Copyright (c) 2014 Marion Ano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
